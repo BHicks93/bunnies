509abe77b2b5e7ae4418cfb67db9eddee709fba9
BRIAN HICKS <bhicks@risd.edu>
2015-04-21 10:43:56 -0400

    uploadAll

---
+++ js/Clone.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	$("body").click(function () {
+		$("#myimage").clone().insertAfter("#myimage:last");
+		$('.bunny').effect('bounce', 500);
+	});
+	$('.bunny').on("mouseenter", function() {
+    	$("#myimage").clone().insertAfter("#myimage:last");
+    	$('.bunny').effect('bounce', 500);
+    });
+	
+	function randomFromTo(from, to){
+			return Math.floor(Math.random() * (to - from + 1) + from);
+		}
+  
+	function moveRandom(obj) {
+			/* get container position and size
+			 * -- access method : cPos.top and cPos.left */
+			var cPos = $('#container').offset();
+			var cHeight = $('#container').height();
+			var cWidth = $('#container').width();
+			
+			// get box padding (assume all padding have same value)
+			var pad = parseInt($('#container').css('padding-top').replace('px', ''));
+			
+			// get movable box size
+			var bHeight = obj.height();
+			var bWidth = obj.width();
+			
+			// set maximum position
+			maxY = cPos.top + cHeight - bHeight - pad;
+			maxX = cPos.left + cWidth - bWidth - pad;
+			
+			// set minimum position
+			minY = cPos.top + pad;
+			minX = cPos.left + pad;
+			
+			// set new position			
+			newY = randomFromTo(minY, maxY);
+			newX = randomFromTo(minX, maxX);
+			
+			obj.animate({
+				top: newY,
+				left: newX
+				}, 5000, function() {
+          			moveRandom(obj);
+          			$('.bunny').effect('bounce', 500);
+			});
+		}
+	$('.bunny').each(function() {
+		moveRandom($('.bunny'));
+	});
+
+
+});
---
+++ js/script.js
@@ -0,0 +1,402 @@
+$(document).ready(function(){
+	//all jQuery code goes here
+	console.log("Hi there buddy");
+	
+	//$('selector').function(actions you want to happen);
+		//jQuary + css selector + action
+	$('#myimage').mouseenter(function() {
+    	$(this).effect('bounce', 500);
+		});
+	$('#myimage').one("mouseenter", function() {
+    		$('#myimage1').css("display", "block")
+    	});
+
+	$('#myimage1').mouseenter(function() {
+    	$(this).effect('bounce', 500);
+		});
+	$('#myimage1').one("mouseenter", function() {
+    		$('#myimage2').css("display", "block")
+    		$('#myimage3').css("display", "block")
+    	});
+
+	$('#myimage2').mouseenter(function() {
+    	$(this).effect('bounce', 500);
+		});
+	$('#myimage2').one("mouseenter", function() {
+    		$('#myimage4').css("display", "block")
+    		$('#myimage5').css("display", "block")
+    		$('#myimage6').css("display", "block")
+    		$('#myimage7').css("display", "block")
+    	});
+
+	$('#myimage3').mouseenter(function() {
+    	$(this).effect('bounce', 500);
+		});
+	$('#myimage3').one("mouseenter", function() {
+    		$('#myimage8').css("display", "block")
+    		$('#myimage9').css("display", "block")
+    		$('#myimage10').css("display", "block")
+    		$('#myimage11').css("display", "block")
+    		$('#myimage12').css("display", "block")
+    		$('#myimage13').css("display", "block")
+    		$('#myimage14').css("display", "block")
+    		$('#myimage15').css("display", "block")
+    	});
+
+	$('#myimage4').mouseenter(function() {
+    	$(this).effect('bounce', 500);
+		});
+	$('#myimage4').one("mouseenter", function() {
+    		$('#myimage16').css("display", "block")
+    		$('#myimage17').css("display", "block")
+    		$('#myimage18').css("display", "block")
+    		$('#myimage19').css("display", "block")
+    		$('#myimage20').css("display", "block")
+    		$('#myimage21').css("display", "block")
+    		$('#myimage22').css("display", "block")
+    		$('#myimage23').css("display", "block")
+    		$('#myimage24').css("display", "block")
+    		$('#myimage25').css("display", "block")
+    		$('#myimage26').css("display", "block")
+    		$('#myimage27').css("display", "block")
+    		$('#myimage28').css("display", "block")
+    		$('#myimage29').css("display", "block")
+    		$('#myimage30').css("display", "block")
+    		$('#myimage31').css("display", "block")
+    	});
+
+	$('#myimage5').mouseenter(function() {
+    	$(this).effect('bounce', 500);
+		});
+	$('#myimage5').one("mouseenter", function() {
+    		$('#myimage32').css("display", "block")
+    		$('#myimage33').css("display", "block")
+    		$('#myimage34').css("display", "block")
+    		$('#myimage35').css("display", "block")
+    		$('#myimage36').css("display", "block")
+    		$('#myimage37').css("display", "block")
+    		$('#myimage38').css("display", "block")
+    		$('#myimage39').css("display", "block")
+    		$('#myimage40').css("display", "block")
+    		$('#myimage41').css("display", "block")
+    		$('#myimage42').css("display", "block")
+    		$('#myimage43').css("display", "block")
+    		$('#myimage44').css("display", "block")
+    		$('#myimage45').css("display", "block")
+    		$('#myimage46').css("display", "block")
+    		$('#myimage47').css("display", "block")
+    		$('#myimage48').css("display", "block")
+    		$('#myimage49').css("display", "block")
+    		$('#myimage50').css("display", "block")
+    		$('#myimage51').css("display", "block")
+    		$('#myimage52').css("display", "block")
+    		$('#myimage53').css("display", "block")
+    		$('#myimage54').css("display", "block")
+    		$('#myimage55').css("display", "block")
+    		$('#myimage56').css("display", "block")
+    		$('#myimage57').css("display", "block")
+    		$('#myimage58').css("display", "block")
+    		$('#myimage59').css("display", "block")
+    		$('#myimage60').css("display", "block")
+    		$('#myimage61').css("display", "block")
+    		$('#myimage62').css("display", "block")
+    		$('#myimage63').css("display", "block")
+    	});
+	$('#myimage6').mouseenter(function() {
+    	$(this).effect('bounce', 500);
+		});
+	$('#myimage6').one("mouseenter", function() {
+			$('#myimage64').css("display", "block")
+    		$('#myimage65').css("display", "block")
+    		$('#myimage66').css("display", "block")
+    		$('#myimage67').css("display", "block")
+    		$('#myimage68').css("display", "block")
+    		$('#myimage69').css("display", "block")
+    		$('#myimage70').css("display", "block")
+    		$('#myimage71').css("display", "block")
+    		$('#myimage72').css("display", "block")
+    		$('#myimage73').css("display", "block")
+    		$('#myimage74').css("display", "block")
+    		$('#myimage75').css("display", "block")
+    		$('#myimage76').css("display", "block")
+    		$('#myimage77').css("display", "block")
+    		$('#myimage78').css("display", "block")
+    		$('#myimage79').css("display", "block")
+    		$('#myimage80').css("display", "block")
+    		$('#myimage81').css("display", "block")
+    		$('#myimage82').css("display", "block")
+    		$('#myimage83').css("display", "block")
+    		$('#myimage84').css("display", "block")
+    		$('#myimage85').css("display", "block")
+    		$('#myimage86').css("display", "block")
+    		$('#myimage87').css("display", "block")
+    		$('#myimage88').css("display", "block")
+    		$('#myimage89').css("display", "block")
+    		$('#myimage90').css("display", "block")
+    		$('#myimage91').css("display", "block")
+    		$('#myimage92').css("display", "block")
+    		$('#myimage93').css("display", "block")
+    		$('#myimage94').css("display", "block")
+    		$('#myimage95').css("display", "block")
+    		$('#myimage96').css("display", "block")
+    		$('#myimage97').css("display", "block")
+    		$('#myimage98').css("display", "block")
+    		$('#myimage99').css("display", "block")
+    		$('#myimage100').css("display", "block")
+    		$('#myimage101').css("display", "block")
+    		$('#myimage102').css("display", "block")
+    		$('#myimage103').css("display", "block")
+    		$('#myimage104').css("display", "block")
+    		$('#myimage105').css("display", "block")
+    		$('#myimage106').css("display", "block")
+    		$('#myimage107').css("display", "block")
+    		$('#myimage108').css("display", "block")
+    		$('#myimage109').css("display", "block")
+    		$('#myimage110').css("display", "block")
+    		$('#myimage111').css("display", "block")
+    		$('#myimage112').css("display", "block")
+    		$('#myimage113').css("display", "block")
+    		$('#myimage114').css("display", "block")
+    		$('#myimage115').css("display", "block")
+    		$('#myimage116').css("display", "block")
+    		$('#myimage117').css("display", "block")
+    		$('#myimage118').css("display", "block")
+    		$('#myimage119').css("display", "block")
+    		$('#myimage120').css("display", "block")
+    		$('#myimage121').css("display", "block")
+    		$('#myimage122').css("display", "block")
+    		$('#myimage123').css("display", "block")
+    		$('#myimage124').css("display", "block")
+    		$('#myimage125').css("display", "block")
+    		$('#myimage126').css("display", "block")
+    		$('#myimage127').css("display", "block")
+    	});
+	$('#myimage7').mouseenter(function() {
+    	$(this).effect('bounce', 500);
+		});
+	$('#myimage7').one("mouseenter", function() {
+			$('#myimage128').css("display", "block")
+    		$('#myimage129').css("display", "block")
+    		$('#myimage130').css("display", "block")
+    		$('#myimage131').css("display", "block")
+    		$('#myimage132').css("display", "block")
+    		$('#myimage133').css("display", "block")
+    		$('#myimage134').css("display", "block")
+    		$('#myimage135').css("display", "block")
+    		$('#myimage136').css("display", "block")
+    		$('#myimage137').css("display", "block")
+    		$('#myimage138').css("display", "block")
+    		$('#myimage139').css("display", "block")
+    		$('#myimage140').css("display", "block")
+    		$('#myimage141').css("display", "block")
+    		$('#myimage142').css("display", "block")
+    		$('#myimage143').css("display", "block")
+    		$('#myimage144').css("display", "block")
+    		$('#myimage145').css("display", "block")
+    		$('#myimage146').css("display", "block")
+    		$('#myimage147').css("display", "block")
+    		$('#myimage148').css("display", "block")
+    		$('#myimage149').css("display", "block")
+    		$('#myimage150').css("display", "block")
+    		$('#myimage151').css("display", "block")
+    		$('#myimage152').css("display", "block")
+    		$('#myimage153').css("display", "block")
+    		$('#myimage154').css("display", "block")
+    		$('#myimage155').css("display", "block")
+    		$('#myimage156').css("display", "block")
+    		$('#myimage157').css("display", "block")
+    		$('#myimage158').css("display", "block")
+    		$('#myimage159').css("display", "block")
+    		$('#myimage160').css("display", "block")
+    		$('#myimage161').css("display", "block")
+    		$('#myimage162').css("display", "block")
+    		$('#myimage163').css("display", "block")
+    		$('#myimage164').css("display", "block")
+    		$('#myimage165').css("display", "block")
+    		$('#myimage166').css("display", "block")
+    		$('#myimage167').css("display", "block")
+    		$('#myimage168').css("display", "block")
+    		$('#myimage169').css("display", "block")
+    		$('#myimage170').css("display", "block")
+    		$('#myimage171').css("display", "block")
+    		$('#myimage172').css("display", "block")
+    		$('#myimage173').css("display", "block")
+    		$('#myimage174').css("display", "block")
+    		$('#myimage175').css("display", "block")
+    		$('#myimage176').css("display", "block")
+    		$('#myimage177').css("display", "block")
+    		$('#myimage178').css("display", "block")
+    		$('#myimage179').css("display", "block")
+    		$('#myimage180').css("display", "block")
+    		$('#myimage181').css("display", "block")
+    		$('#myimage182').css("display", "block")
+    		$('#myimage183').css("display", "block")
+    		$('#myimage184').css("display", "block")
+    		$('#myimage185').css("display", "block")
+    		$('#myimage186').css("display", "block")
+    		$('#myimage187').css("display", "block")
+    		$('#myimage188').css("display", "block")
+    		$('#myimage189').css("display", "block")
+    		$('#myimage190').css("display", "block")
+    		$('#myimage191').css("display", "block")
+    		$('#myimage192').css("display", "block")
+    		$('#myimage193').css("display", "block")
+    		$('#myimage194').css("display", "block")
+    		$('#myimage195').css("display", "block")
+    		$('#myimage196').css("display", "block")
+    		$('#myimage197').css("display", "block")
+    		$('#myimage198').css("display", "block")
+    		$('#myimage199').css("display", "block")
+    		$('#myimage200').css("display", "block")
+    		$('#myimage201').css("display", "block")
+    		$('#myimage202').css("display", "block")
+    		$('#myimage203').css("display", "block")
+    		$('#myimage204').css("display", "block")
+    		$('#myimage205').css("display", "block")
+    		$('#myimage206').css("display", "block")
+    		$('#myimage207').css("display", "block")
+    		$('#myimage208').css("display", "block")
+    		$('#myimage209').css("display", "block")
+    		$('#myimage210').css("display", "block")
+    		$('#myimage211').css("display", "block")
+    		$('#myimage212').css("display", "block")
+    		$('#myimage213').css("display", "block")
+    		$('#myimage214').css("display", "block")
+    		$('#myimage215').css("display", "block")
+    		$('#myimage216').css("display", "block")
+    		$('#myimage217').css("display", "block")
+    		$('#myimage218').css("display", "block")
+    		$('#myimage219').css("display", "block")
+    		$('#myimage220').css("display", "block")
+    		$('#myimage221').css("display", "block")
+    		$('#myimage222').css("display", "block")
+    		$('#myimage223').css("display", "block")
+    		$('#myimage224').css("display", "block")
+    		$('#myimage225').css("display", "block")
+    		$('#myimage226').css("display", "block")
+    		$('#myimage227').css("display", "block")
+    		$('#myimage228').css("display", "block")
+    		$('#myimage229').css("display", "block")
+    		$('#myimage230').css("display", "block")
+    		$('#myimage231').css("display", "block")
+    		$('#myimage232').css("display", "block")
+    		$('#myimage233').css("display", "block")
+    		$('#myimage234').css("display", "block")
+    		$('#myimage235').css("display", "block")
+    		$('#myimage236').css("display", "block")
+    		$('#myimage237').css("display", "block")
+    		$('#myimage238').css("display", "block")
+    		$('#myimage238').css("display", "block")
+    		$('#myimage240').css("display", "block")
+    		$('#myimage241').css("display", "block")
+    		$('#myimage242').css("display", "block")
+    		$('#myimage243').css("display", "block")
+    		$('#myimage244').css("display", "block")
+    		$('#myimage245').css("display", "block")
+    		$('#myimage246').css("display", "block")
+    		$('#myimage247').css("display", "block")
+    		$('#myimage248').css("display", "block")
+    		$('#myimage249').css("display", "block")
+    		$('#myimage250').css("display", "block")
+    		$('#myimage251').css("display", "block")
+    		$('#myimage252').css("display", "block")
+    		$('#myimage253').css("display", "block")
+    		$('#myimage254').css("display", "block")
+    		$('#myimage255').css("display", "block")
+    	});
+	$('#myimage8').mouseenter(function() {
+    	$(this).effect('bounce', 500);
+		});
+	$('#myimage8').one("mouseenter", function() {
+    		$('#myimage256').css("display", "block")
+    		$('#myimage257').css("display", "block")
+    		$('#myimage258').css("display", "block")
+    		$('#myimage259').css("display", "block")
+    		$('#myimage260').css("display", "block")
+    		$('#myimage261').css("display", "block")
+    		$('#myimage262').css("display", "block")
+    		$('#myimage263').css("display", "block")
+    		$('#myimage264').css("display", "block")
+    		$('#myimage265').css("display", "block")
+    		$('#myimage266').css("display", "block")
+    		$('#myimage267').css("display", "block")
+    		$('#myimage268').css("display", "block")
+    		$('#myimage269').css("display", "block")
+    		$('#myimage270').css("display", "block")
+    		$('#myimage271').css("display", "block")
+    		$('#myimage272').css("display", "block")
+    		$('#myimage273').css("display", "block")
+    		$('#myimage274').css("display", "block")
+    		$('#myimage275').css("display", "block")
+    		$('#myimage276').css("display", "block")
+    		$('#myimage277').css("display", "block")
+    		$('#myimage278').css("display", "block")
+    		$('#myimage279').css("display", "block")
+    		$('#myimage280').css("display", "block")
+    		$('#myimage281').css("display", "block")
+    		$('#myimage282').css("display", "block")
+    		$('#myimage283').css("display", "block")
+    		$('#myimage284').css("display", "block")
+    		$('#myimage285').css("display", "block")
+    		$('#myimage286').css("display", "block")
+    		$('#myimage287').css("display", "block")
+    		$('#myimage288').css("display", "block")
+    		$('#myimage289').css("display", "block")
+    		$('#myimage290').css("display", "block")
+    		$('#myimage291').css("display", "block")
+    		$('#myimage292').css("display", "block")
+    		$('#myimage293').css("display", "block")
+    		$('#myimage294').css("display", "block")
+    		$('#myimage295').css("display", "block")
+    		$('#myimage296').css("display", "block")
+    		$('#myimage297').css("display", "block")
+    		$('#myimage298').css("display", "block")
+    		$('#myimage299').css("display", "block")
+    		$('#myimage300').css("display", "block")
+
+
+    	});
+
+
+		function randomFromTo(from, to){
+			return Math.floor(Math.random() * (to - from + 1) + from);
+		}
+  
+		function moveRandom(obj) {
+			/* get container position and size
+			 * -- access method : cPos.top and cPos.left */
+			var cPos = $('#container').offset();
+			var cHeight = $('#container').height();
+			var cWidth = $('#container').width();
+			
+			// get box padding (assume all padding have same value)
+			var pad = parseInt($('#container').css('padding-top').replace('px', ''));
+			
+			// get movable box size
+			var bHeight = obj.height();
+			var bWidth = obj.width();
+			
+			// set maximum position
+			maxY = cPos.top + cHeight - bHeight - pad;
+			maxX = cPos.left + cWidth - bWidth - pad;
+			
+			// set minimum position
+			minY = cPos.top + pad;
+			minX = cPos.left + pad;
+			
+			// set new position			
+			newY = randomFromTo(minY, maxY);
+			newX = randomFromTo(minX, maxX);
+			
+			obj.animate({
+				top: newY,
+				left: newX
+				}, 10000, function() {
+          			moveRandom(obj);
+			});
+		}
+	$('.bunny').each(function() {
+		moveRandom($(this));
+	});
+	
+});
